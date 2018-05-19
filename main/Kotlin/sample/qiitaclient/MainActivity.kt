fe52e65bf2ec865a07abb4f34b737a0615ac5147
MasahiroTakahashi <take1931032@gmail.com>
2018-05-19 11:32:27 +0900

    masa

--- main/Kotlin/sample/qiitaclient/MainActivity.kt
+++ main/Kotlin/sample/qiitaclient/MainActivity.kt
@@ -2,14 +2,17 @@ package sample.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.DialogTitle
 import android.view.View
-import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
+import android.widget.ProgressBar
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 import sample.qiitaclient.client.ArticleClient
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
@@ -17,6 +20,8 @@ import sample.qiitaclient.view.ArticleView
 
 
 class MainActivity : AppCompatActivity() {
+    val queryEditText =findViewById(R.id.query_edit_text) as EditText
+    val searchButton = findViewById(R.id.search_button) as Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +34,7 @@ class MainActivity : AppCompatActivity() {
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
         val articleClient = retrofit.create(ArticleClient::class.java)
-        
+
 
         setContentView(R.layout.activity_main)
         val listAdapter = ArticleListAdapter(applicationContext)
@@ -42,6 +47,19 @@ class MainActivity : AppCompatActivity() {
             val article =listAdapter.articles[position]
             ArticleActivity.intent(this,article).let { startActivity(it) }
         }
+
+        searchButton.setOnClickListener{
+            articleClient.search(queryEditText.text.toString())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        queryEditText.text.clear()
+                        listAdapter.articles=it
+                        listAdapter.notifyDataSetChanged()
+                    }, {
+                        toast("エラー: $it")
+                    })
+        }
 //      Articleオブジェクトの生成
 //        val articleView = ArticleView(applicationContext)
 //
@@ -49,9 +67,6 @@ class MainActivity : AppCompatActivity() {
 //                title = "Kotlin入門",
 //                url = "http://www.example.com/articles/123",
 //                user = User(id="456",name="たろう",profileImageUrl = "")))
-//
-//
-//
 //        setContentView(articleView)
     }
     private fun dummyArticle(title: String , userName: String ): Article =
