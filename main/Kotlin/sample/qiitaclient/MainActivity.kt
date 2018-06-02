808edf485267e366564631915a4c95fff9eef973
MasahiroTakahashi <take1931032@gmail.com>
2018-06-02 19:29:31 +0900

    masa

--- main/Kotlin/sample/qiitaclient/MainActivity.kt
+++ main/Kotlin/sample/qiitaclient/MainActivity.kt
@@ -2,12 +2,15 @@ package sample.qiitaclient
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
@@ -17,11 +20,16 @@ import sample.qiitaclient.model.User
 
 
 class MainActivity : AppCompatActivity() {
-    val queryEditText =findViewById(R.id.query_edit_text) as EditText
-    val searchButton = findViewById(R.id.search_button) as Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        val listAdapter = ArticleListAdapter(applicationContext)
+        val queryEditText =findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button) as Button
 
         val gson = GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
@@ -29,31 +37,33 @@ class MainActivity : AppCompatActivity() {
         val retrofit = Retrofit.Builder()
                 .baseUrl("https://qiita.com")
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build()
         val articleClient = retrofit.create(ArticleClient::class.java)
 
-
-        setContentView(R.layout.activity_main)
-        val listAdapter = ArticleListAdapter(applicationContext)
-        listAdapter.articles = listOf(dummyArticle("Kotlin入門","たろう"),
-                dummyArticle("JAVA入門","二郎"))
-
-        val listView: ListView = findViewById(R.id.list_view) as ListView
-        listView.adapter = listAdapter
-        listView .setOnItemClickListener{ adapterView, View, position, id ->
-            val article =listAdapter.articles[position]
-            ArticleActivity.intent(this,article).let { startActivity(it) }
-        }
+//        listAdapter.articles = listOf(dummyArticle("Kotlin入門","たろう"),
+//                dummyArticle("JAVA入門","二郎"))
+//
+//
+//        listView.adapter = listAdapter
+//        listView .setOnItemClickListener{ adapterView, View, position, id ->
+//            val article =listAdapter.articles[position]
+//            ArticleActivity.intent(this,article).let { startActivity(it) }
+//        }
 
         searchButton.setOnClickListener{
             articleClient.search(queryEditText.text.toString())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .doAfterTerminate {
+                        progress_bar.visibility = View.GONE
+                    }
+//                    .bindToLifecycle(this)
                     .subscribe({
                         queryEditText.text.clear()
                         listAdapter.articles = it
                         listAdapter.notifyDataSetChanged()
-                    },{
+                    }, {
                         toast("エラー: $it")
                     })
         }
